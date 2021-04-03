5277cf68e911bb5cc6ea4fcd0538c9d4615ce97f
Ray <raytommy1234@gmail.com>
2021-04-03 13:09:30 +0800

    Adding Camera Feature

--- App.js
+++ App.js
@@ -1,21 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const firebaseConfig = {
+  apiKey: "AIzaSyBhfk-lkhb2xsLCcmo0qNjx4bH0HCGa5I0",
+  authDomain: "instagramclone-652ac.firebaseapp.com",
+  projectId: "instagramclone-652ac",
+  storageBucket: "instagramclone-652ac.appspot.com",
+  messagingSenderId: "1064486268164",
+  appId: "1:1064486268164:web:68b9ca35b97de993fd7451",
+  measurementId: "G-HLGL04JGBJ"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import MainScreen from './components/Main';
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
+
+
+
+import { disableExpoCliLogging } from 'expo/build/logs/Logs';
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} navigation={this.props.navigation} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
+
+  }
+}
+
+export default App;
